51b0bcfe224b1314ec003481a7283e03ac3604db
sohanswamy5511-del <sohanswamy5511@gmail.com>
2026-03-05 15:58:54 +0000

    Now you can order toppings

--- combo_menu.py
+++ combo_menu.py
@@ -10,3 +10,38 @@ elif flavor == "chocolate":
     total_cost = 3.25
 elif flavor == "strawberry":
     total_cost = 3.50
+YesorNoToppings = input("Would you like toppings? ")
+YesorNoToppings = YesorNoToppings.lower()
+ToppingsList = ["yes", "no"]
+while YesorNoToppings not in ToppingsList:
+    YesorNoToppings = input("Please enter yes or no: ").lower()
+if YesorNoToppings == "yes":
+    ToppingSize = input("What size topping portion would you like? Small ($0.50), Medium ($0.75), or Large ($1.00)? ")
+    ToppingSize = ToppingSize.lower()
+    ToppingSizes = ["small", "medium", "large"]
+    while ToppingSize not in ToppingSizes:
+        ToppingSize = input("Please enter small, medium, or large: ").lower()
+    if ToppingSize == "small":
+        total_cost += 0.50
+        MegaSize = input("Would you like to mega size your toppings for an extra $0.50? ")
+        MegaSize = MegaSize.lower()
+        while MegaSize not in ["yes", "no"]:
+            MegaSize = input("Please enter yes or no: ").lower()
+        if MegaSize == "yes":
+            total_cost += 0.50
+            ToppingSize = "large"
+        elif MegaSize == "no":
+            ToppingSize = "small"
+            pass
+        else:
+            Megasize = input("Please enter yes or no: ").lower()
+    elif ToppingSize == "medium":
+        total_cost += 0.75
+    elif ToppingSize == "large":
+        total_cost += 1.00
+    Topping = input("What topping would you like? Sprinkles, chocolate chips, or gummy bears? ")
+    Topping = Topping.lower()
+    while Topping not in ["sprinkles", "chocolate chips", "gummy bears"]:
+        Topping = input("Please enter sprinkles, chocolate chips, or gummy bears: ").lower()
+else:
+    Topping = ""
